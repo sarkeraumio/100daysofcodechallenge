29ca92a3ff0416fa4e9b39cf33ab3557de9fa3f8
Aumio Kumar Sarker <87389689+sarkeraumio@users.noreply.github.com>
2022-03-02 23:48:36 +0600

    Add files via upload

---
+++ practice.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int n, i;
+
+    scanf("%d", &n);
+
+    i=1;
+
+    while(i<=n){
+
+        printf(" %d", i );
+
+        i=i+1;
+    }
+
+
+    return 0;
+}
